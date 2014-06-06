dc3f98bc2522802af8e01abd4762219b6ed76f61
Shaun Henderson <shaun.henderson@fifthring.com>
2014-06-06 13:12:37 +0100

    Styling added to navigation

--- app/views/layouts/scaffold.blade.php
+++ app/views/layouts/scaffold.blade.php
@@ -2,14 +2,12 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/app.css">
-    <style>
-    </style>
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 </head>
 
 <body>
 
-<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="navbar navbar-default navbar-static-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -18,37 +16,21 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">BAMP</a>
+          <a class="navbar-brand" href="/products">Fifth Ring</a>
         </div>
+
         <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Default</a></li>
-            <li class="active"><a href="./">Static top</a></li>
-            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+            <li class="active"><a href="/products">Products</a></li>
+            <li><a href="/product_categories">Categories</a></li>
+            <li><a href="/companies">Companies</a></li>
+            <li><a href="/people">People</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </div>
 
 <div class="container">
-
     <div class="row">
         <div class="col-md-12">
 
@@ -63,6 +45,11 @@
         </div>
     </div>
 </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ URL::asset('js/vendor/bootstrap.min.js') }}"></script>
 
 </body>
 </html>
